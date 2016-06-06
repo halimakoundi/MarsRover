2dd7a1537b6a37e5788567f1f8dfd5a78dd1daf2
Halima KOUNDI <hkoundi@gmail.com>
2016-06-06 16:38:55 +0100

    fake test

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -9,7 +9,8 @@ namespace MarsRover.Tests
         [Test]
         public void return_initial_position_with_empty_command()
         {
-
+            var position = "0,0,N";
+            Assert.That(position, Is.EqualTo("0,0,N"));
         }
     }
 }
