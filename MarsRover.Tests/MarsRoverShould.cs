82a33613ba3721cee4e65e78c9e16877299045d6
Halima KOUNDI <hkoundi@gmail.com>
2016-06-06 16:37:38 +0100

    first set up

---
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class MarsRoverShould
+    {
+        [Test]
+        public void return_initial_position_with_empty_command()
+        {
+
+        }
+    }
+}
