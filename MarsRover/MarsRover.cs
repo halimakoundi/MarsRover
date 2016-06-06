e9a39cacfc6e2c580967403b567dbe1b440c3d30
Halima KOUNDI <hkoundi@gmail.com>
2016-06-06 16:52:50 +0100

    move one time returns 0,1,N

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -26,7 +26,8 @@ namespace MarsRover
         {
             if (command == "M")
             {
-                return "0,1,N";
+                var position = "0,1,N";
+                return position;
             }
             return "0,0,N";
         }
